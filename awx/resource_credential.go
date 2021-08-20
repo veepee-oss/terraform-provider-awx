545df0a0711fd1e6e7f141d050e9050610e69c20
Nick Gray <ngray@pindrop.com>
2021-08-20 15:43:30 -0400

    Trying a generic credential type

---
+++ awx/resource_credential.go
@@ -0,0 +1,143 @@
+/*
+*TBD*
+
+Example Usage
+
+```hcl
+*TBD*
+```
+
+*/
+package awx
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	awx "github.com/mrcrilly/goawx/client"
+)
+
+func resourceCredential() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceCredentialCreate,
+		ReadContext:   resourceCredentialRead,
+		UpdateContext: resourceCredentialUpdate,
+		DeleteContext: CredentialsServiceDeleteByID,
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"organisation_id": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"credential_type_id": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+				Description: "Specify the type of credential you want to create. Refer to the Ansible Tower documentation for details on each type",
+			},
+			"inputs": &schema.Schema{
+				Type: schema.TypeString,
+				Required: true,
+				Sensitive: true,
+			},
+		},
+	}
+}
+
+func resourceCredentialCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	var err error
+
+	newCredential := map[string]interface{}{
+		"name":            d.Get("name").(string),
+		"description":     d.Get("description").(string),
+		"organisation_id": d.Get("organisation_id").(int),
+		"credential_type": d.Get("credential_type_id"),
+		"inputs":          normalizeJsonYaml(d.Get("inputs")),
+	}
+
+	client := m.(*awx.AWX)
+	cred, err := client.CredentialsService.CreateCredentials(newCredential, map[string]string{})
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Unable to create new credential",
+			Detail:   fmt.Sprintf("Unable to create new credential: %s", err.Error()),
+		})
+		return diags
+	}
+
+	d.SetId(strconv.Itoa(cred.ID))
+	resourceCredentialRead(ctx, d, m)
+
+	return diags
+}
+
+func resourceCredentialRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	client := m.(*awx.AWX)
+	id, _ := strconv.Atoi(d.Id())
+	cred, err := client.CredentialsService.GetCredentialsByID(id, map[string]string{})
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Unable to fetch credential",
+			Detail:   fmt.Sprintf("Unable to credential with id %d: %s", id, err.Error()),
+		})
+		return diags
+	}
+
+	d.Set("name", cred.Name)
+	d.Set("description", cred.Description)
+	d.Set("organisation_id", cred.OrganizationID)
+	d.Set("inputs", cred.Inputs)
+
+	return diags
+}
+
+func resourceCredentialUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	keys := []string{
+		"name",
+		"description",
+		"inputs",
+		"organisation_id",
+	}
+
+	if d.HasChanges(keys...) {
+		var err error
+
+		id, _ := strconv.Atoi(d.Id())
+		updatedCredential := map[string]interface{}{
+			"name":            d.Get("name").(string),
+			"description":     d.Get("description").(string),
+			"organization":    d.Get("organisation_id").(int),
+			"credential_type": d.Get("credential_type_id"),
+			"inputs":          normalizeJsonYaml(d.Get("inputs")),
+		}
+
+		client := m.(*awx.AWX)
+		_, err = client.CredentialsService.UpdateCredentialsByID(id, updatedCredential, map[string]string{})
+		if err != nil {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "Unable to update existing credentials",
+				Detail:   fmt.Sprintf("Unable to update existing credentials with id %d: %s", id, err.Error()),
+			})
+			return diags
+		}
+	}
+
+	return resourceCredentialSCMRead(ctx, d, m)
+}
