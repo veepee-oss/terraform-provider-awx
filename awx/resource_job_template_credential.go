bd37dc025e8f02ea1457ff36408760b7fd1814a2
antoine.leveugle <antoine.leveugle@gmail.com>
2021-12-21 21:57:31 +0100

    docs: fix typo in resource name

--- awx/resource_job_template_credential.go
+++ awx/resource_job_template_credential.go
@@ -4,7 +4,7 @@
 Example Usage
 
 ```hcl
-resource "awx_job_template_credentials" "baseconfig" {
+resource "awx_job_template_credential" "baseconfig" {
   job_template_id = awx_job_template.baseconfig.id
   credential_id   = awx_credential_machine.pi_connection.id
 }
